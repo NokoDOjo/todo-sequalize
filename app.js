088cd0ba8fae7e56ebbc635aca5be0ee6217a8ac
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-08-20 10:40:35 +0800

    feat: add auth & facebook login

--- app.js
+++ app.js
@@ -2,24 +2,22 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
-const bcrypt = require('bcryptjs')
-const passport = require('passport')
 const usePassport = require('./config/passport')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
-const PORT = 3000
-
+const PORT = process.env.PORT
 const routes = require('./routes')
-
-const db = require('./models')
-const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
@@ -27,7 +25,6 @@ app.use(session({
 usePassport(app)
 
 app.use(routes)
-
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
