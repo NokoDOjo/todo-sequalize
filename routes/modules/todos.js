5fda2c3614879652aff64bba7bf0c1ead755226b
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-08-20 10:09:02 +0800

    feat: modify RESTFUL routes

---
+++ routes/modules/todos.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../../models')
+const Todo = db.Todo
+
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name, userId })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findOne({
+    where: { id }
+  })
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(err => console.log(err))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findOne({ where: { id }})
+    .then(todo => res.render('edit', { todo: todo.get() }))
+    .catch(err => console.log(err))
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { name, isDone } = req.body
+  console.log(req.body)
+  return Todo.findOne({ where: { id }})
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(err => console.log(err))
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+
+  return Todo.findOne({ where: { id } })
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
+module.exports = router
